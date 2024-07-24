c3e985fd156f927fd8f75b405145d6539e2f8feb
irakeshbind <rakeshbind351998@gmail.com>
2024-07-24 08:19:03 +0530

    Nav section complete

--- toggle.js
+++ toggle.js
@@ -1,8 +1,8 @@
-const bar= document.getElementById("bar")
+const bar=document.getElementById("bar")
 const menu=document.getElementById("menu")
 
 if(bar){
     bar.addEventListener("click",()=>{
-        menu.classList.toggle("active")
-    }); 
+          menu.classList.toggle("active");
+    });
 }
